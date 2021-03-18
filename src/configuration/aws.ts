ef7d98b9e3f33451d13e7fb6c23c1fcdff0eebb6
Bernardo Montoya <ber.montoya12@gmail.com>
2021-03-17 22:49:43 -0600

    feat: configure aws amplify

---
+++ src/configuration/aws.ts
@@ -0,0 +1,14 @@
+const config = {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://zvcn05c2c6.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_NpRSJ0LEZ",
+    APP_CLIENT_ID: "6qdl0l846lpiqcc0hon51qifl2",
+    IDENTITY_POOL_ID: "us-east-1:f059066f-b0ee-4a1b-a588-732f8f8347aa",
+  },
+};
+
+export default config;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Amplify } from "aws-amplify";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import config from "./configuration/aws";
+import App from "./App";
 import DryEyeTheme from "./theme/theme";
 import "./index.css";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "admin",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 const theme = extendTheme(DryEyeTheme);
 
